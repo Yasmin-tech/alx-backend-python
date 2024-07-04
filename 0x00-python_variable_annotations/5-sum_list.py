8ea97b7a2627b5a6ddb5374a791b90de8b7d7233
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-04 16:42:15 +0300

    add task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' This model contains a type-annotated function sum_list:
+    It takes a list input_list of floats as argument
+    returns their sum as a float.
+    '''
+
+
+def sum_list(input_list: list[float]) -> float:
+    '''
+        This function takes a list of floats and returns their sum
+        '''
+    return sum(input_list)
