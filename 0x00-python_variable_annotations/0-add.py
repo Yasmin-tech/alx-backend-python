f993e17b6ad39c1f6980919df7746332348a8fac
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-04 14:50:11 +0300

    add task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' This model contains the type-annotated function add:
+    takes a float a and a float b as arguments
+    and returns their sum as a float
+    '''
+
+
+def add(a: float, b: float) -> float:
+    ''' return the sum of a and b
+        a and b they should be a float type
+        '''
+    return a + b
