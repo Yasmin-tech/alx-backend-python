1b2fa6d1150699090d71e71bae6c9c118b12f97a
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-04 16:10:29 +0300

    add task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' This model contains the type-annotated function concat:
+    takes a string str1 and a string str2 as arguments
+    and return a concatenated string
+    '''
+
+
+def concat(str1: str, str2: str) -> str:
+    ''' return a concatenated string str1 + str2
+        str1 and str2 they should be a string type
+        '''
+    return str1 + str2
