72f90e56689862e1b2e2a6b679471243f0dae1bd
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-04 16:35:42 +0300

    add task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' This model contains some Defined and annotated variables
+    '''
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
