488f20dd9341b4621b083ffe6762e7d945544742
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-04 22:37:35 +0300

    add task 12

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+''' Fix mypy
+    '''
+
+
+from typing import Tuple, List, Any, Sequence, Union
+
+
+def zoom_array(lst: Sequence[Any], factor: Union[int, float] = 2) -> List[Any]:
+    ''' Fix mypy
+        '''
+    factor = int(factor)
+    zoomed_in: List[Any] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+print(zoom_2x)
+
+zoom_3x = zoom_array(array, 3.0)
+print(zoom_3x)
