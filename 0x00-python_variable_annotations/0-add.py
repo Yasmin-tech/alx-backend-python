5e68b4fb7220d02d705f68029e79be02226b6372
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-04 15:56:18 +0300

    fix the shebang

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 ''' This model contains the type-annotated function add:
     takes a float a and a float b as arguments
     and returns their sum as a float
