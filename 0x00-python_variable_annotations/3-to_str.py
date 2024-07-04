9b5518e5558bb146811f34464a61dafdf8c92b25
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-04 16:30:40 +0300

    add task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+''' This model contains the type-annotated function to_str:
+    takes a float n as it argument
+    and return the string representation of the float
+    '''
+
+
+def to_str(n: float) -> str:
+    ''' rwturn the string representation of the float
+        '''
+    return str(n)
