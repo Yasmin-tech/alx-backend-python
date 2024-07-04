c8dbcb6acb3924489ae7193cd59f09c1c135cb75
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-04 17:25:14 +0300

    add task 7

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+''' This model contains the type-annotated function make_multiplier.
+    It takes an argument multiplier and
+    return a function that squares the multiplier
+    '''
+
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    ''' return the Callable function '''
+    def inner_function(multiplier: float) -> float:
+        ''' This is the function that should be returned '''
+        return multiplier * multiplier
+    return inner_function
