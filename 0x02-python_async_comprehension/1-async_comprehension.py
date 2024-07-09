e0d21f6236ce4e4cb248a75267ed3bdd8020042e
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-09 19:25:00 +0300

    add task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+''' Import async_generator from the previous task and
+    then write a coroutine called async_comprehension
+    '''
+
+
+import asyncio
+from typing import List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    ''' Async Coroutine that will create a list resulted
+        from calling async_generator using list comprehension
+        '''
+
+    return [item async for item in async_generator()]
