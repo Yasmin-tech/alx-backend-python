02fdfa272f960725657ecfd54aa4b2ba297f1f95
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-07-09 10:53:57 +0300

    task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+''' Implement the coroutine wait_n that calls wait_random
+    n times concurrently
+    '''
+
+
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    ''' spawn wait_random n times with the specified max_dela
+        '''
+
+    # opp = [wait_random(max_delay) for _ in range(n)]
+    tasks = asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+    await tasks
+    return sorted(tasks.result())
